0bc6ad080b32f626bb871df39f1cf3b0041f16bf
Jando <alexandroguterez@gmail.com>
2022-05-17 14:32:59 -0700

    make popup form for adding books

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = false;
     this.info = function() { 
         let sentence =  title + " by " + author + ", " + pages + ", ";
         if(!this.read) sentence += "not read yet";
@@ -11,3 +11,41 @@ function Book(title, author, pages, read) {
     }
 }
 
+let myLibrary = [];
+
+function addBookToLibrary(title, author, pages) {
+    myLibrary.push(new Book(title, author, pages));
+}
+
+addBookToLibrary("A test of tests", "Epstein", 69)
+addBookToLibrary("The book of books", "Jeff Valorant", 420)
+addBookToLibrary("The book of books", "Jeff Valorant", 420)
+addBookToLibrary("The book of books", "Jeff Valorant", 420)
+addBookToLibrary("The book of books", "Jeff Valorant", 420)
+addBookToLibrary("The book of books", "Jeff Valorant", 420)
+addBookToLibrary("A unique book", "JandoTopia.com", 420)
+addBookToLibrary("The book of books", "Jeff Valorant", 420)
+addBookToLibrary("The book of books", "Jeff Valorant", 420)
+
+
+const shelve = document.getElementsByClassName("shelve");
+
+document.querySelector("img").addEventListener('click', ()=> {
+
+});
+for(let i = 0; i < myLibrary.length; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = `<h3>${myLibrary[i].title}<h3> 
+    <h3>by ${myLibrary[i].author}<h3>
+    <p>${myLibrary[i].pages} pages<p>`;
+    shelve[0].appendChild(newDiv);
+}
+
+document.getElementById('add').addEventListener('click', ()=> {
+    document.getElementById('form-popup').classList.remove('hidden');
+    document.getElementById('overlay').classList.remove('hidden');
+    });
+document.getElementById('overlay').addEventListener('click', ()=> {
+    document.getElementById('form-popup').classList.add('hidden');
+    document.getElementById('overlay').classList.add('hidden');
+    });
