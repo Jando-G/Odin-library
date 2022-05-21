8932edc11f318f8d5f4b1e294618d27be99b616f
Jando <alexandroguterez@gmail.com>
2022-05-21 09:39:45 -0700

    add delete function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,15 @@
+function generateRandomColor() {
+    var letters = '12345678'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return color;
+  }
+
 function Book(title, author, pages) {
     this.title = title;
+    this.color = generateRandomColor();
     this.author = author;
     this.pages = pages;
     this.read = false;
@@ -21,13 +31,30 @@ function refreshShelf() {
     }       
     for(let i = 0; i < myLibrary.length; i++) {
         let newDiv = document.createElement("div");
-        newDiv.innerHTML = `<h3>${myLibrary[i].title}<h3> 
-        <h3>by ${myLibrary[i].author}<h3>
-        <p>${myLibrary[i].pages} pages<p>`;
+        newDiv.innerHTML = `<div>
+        <p class="pages ${myLibrary[i].read ? 'pages-active' : ''}">${myLibrary[i].pages}</p>
+        <img class="delete" src="./Assets/delete-outline.png">
+        </div>
+        <h2>${myLibrary[i].title}</h2> 
+        <h3>by ${myLibrary[i].author}</h3>`;
+        newDiv.style.backgroundColor = myLibrary[i].color;
         shelve.appendChild(newDiv);
     }
+    let pages = document.getElementsByClassName("pages");
+for(let i = 0; i < pages.length; i++) {
+    pages[i].addEventListener('click', ()=> {
+        myLibrary[i].read = !myLibrary[i].read;
+        pages[i].classList.toggle("pages-active");
+    })
 }
-
+    let deletes = document.getElementsByClassName("delete");
+    for(let i = 0; i < deletes.length; i++) {
+        deletes[i].addEventListener('click', ()=> {
+            myLibrary.splice(i, 1);
+            refreshShelf();
+        })
+    }
+}
 function addBook(title, author, pages) {
     myLibrary.push(new Book(title, author, pages));
 }
@@ -49,7 +76,7 @@ document.getElementById('submit').addEventListener('click', ()=> {
         addBook(document.getElementById('title').value,
         document.getElementById('author').value,
         document.getElementById('pages').value);
-
+        
         refreshShelf();
     }
 });
@@ -57,6 +84,7 @@ document.getElementById('add').addEventListener('click', ()=> {
     document.getElementById('form-popup').classList.remove('hidden');
     document.getElementById('overlay').classList.remove('hidden');
     });
+
 document.getElementById('overlay').addEventListener('click', ()=> {
     document.getElementById('form-popup').classList.add('hidden');
     document.getElementById('overlay').classList.add('hidden');
